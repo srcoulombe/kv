f2cd29b7082d69268fdd41e1e23614fba3434aa1
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2026-07-01 11:26:32 -0400

    wrap up 'https://elixir.hexdocs.pm/agents.html' chapter

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -6,4 +6,6 @@ defmodule KV.Bucket do
   def get(bucket, key), do: Agent.get(bucket, &Map.get(&1, key))
 
   def put(bucket, key, value), do: Agent.update(bucket, &Map.put(&1, key, value))
+
+  def delete(bucket, key), do: Agent.get_and_update(bucket, &Map.pop(&1, key))
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -8,6 +8,23 @@ defmodule KV.BucketTest do
 
       KV.Bucket.put(bucket, "milk", 3)
       assert KV.Bucket.get(bucket, "milk") == 3
-    end  
+    end
+
+    test "stores key:value pairs on a named process" do
+      {:ok, _} = KV.Bucket.start_link(name: :shopping_list)
+      assert KV.Bucket.get(:shopping_list, "milk") == nil
+
+      KV.Bucket.put(:shopping_list, "milk", 3)
+      assert KV.Bucket.get(:shopping_list, "milk") == 3
+    end
+
+    test "deletes key:value pair" do
+      {:ok, bucket} = KV.Bucket.start_link([])
+      KV.Bucket.put(bucket, "milk", 3)
+      assert KV.Bucket.get(bucket, "milk") == 3
+
+      KV.Bucket.delete(bucket, "milk")
+      assert KV.Bucket.get(bucket, "milk") == nil
+    end   
   end
 end

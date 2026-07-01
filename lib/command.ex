59b7f0f610a2d3e2ad74580f37007437221c3d8e
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2026-07-01 14:31:19 -0400

    create Command module and doc tests

---
+++ lib/command.ex
@@ -0,0 +1,41 @@
+defmodule KV.Command do
+    @doc ~S"""
+  Parses the given `line` into a command.
+
+  ## Examples
+
+      iex> KV.Command.parse "CREATE shopping\r\n"
+      {:ok, {:create, "shopping"}}
+
+      iex> KV.Command.parse "CREATE  shopping  \r\n"
+      {:ok, {:create, "shopping"}}
+
+      iex> KV.Command.parse "PUT shopping milk 1\r\n"
+      {:ok, {:put, "shopping", "milk", "1"}}
+
+      iex> KV.Command.parse "GET shopping milk\r\n"
+      {:ok, {:get, "shopping", "milk"}}
+
+      iex> KV.Command.parse "DELETE shopping eggs\r\n"
+      {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commands with the wrong number of
+  arguments return an error:
+
+      iex> KV.Command.parse "UNKNOWN shopping eggs\r\n"
+      {:error, :unknown_command}
+
+      iex> KV.Command.parse "GET shopping\r\n"
+      {:error, :unknown_command}
+
+  """
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+end

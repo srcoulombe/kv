b9d7226805267acdda65037c8549882a2cb0a9ef
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2026-07-01 12:11:41 -0400

    allow dynamic creation of Buckets by introducing DynamicSupervisor

--- lib/kv.ex
+++ lib/kv.ex
@@ -4,11 +4,20 @@ defmodule KV do
   @impl true
   def start(_type, _args) do
     children = [
-      {Registry, name: KV, keys: :unique}
+      {Registry, name: KV, keys: :unique},
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
     ]
     Supervisor.start_link(
       children,
       strategy: :one_for_one
     )
   end
+
+  def create_bucket(name) do
+    DynamicSupervisor.start_child(KV.BucketSupervisor, {KV.Bucket, name: via(name)})
+  end
+
+  def lookup_bucket(name), do: GenServer.whereis(via(name))
+
+  defp via(name), do: {:via, Registry, {KV, name}}
 end
